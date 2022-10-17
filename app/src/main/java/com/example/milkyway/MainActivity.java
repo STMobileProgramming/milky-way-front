c1ac5f780dac105a1de0c69cf11fe3965540e4a0
yubin <dbqls2821@gmail.com>
2022-10-18 00:43:08 +0900

    feat: bottom navigation bar

--- app/src/main/java/com/example/milkyway/MainActivity.java
+++ app/src/main/java/com/example/milkyway/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.milkyway;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    NavigationBarView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 바텀 네비게이션 바 관련
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+        getSupportFragmentManager().beginTransaction().add(R.id.main_frame, new HomeFragment()).commit();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new HomeFragment()).commit();
+                        break;
+                    case R.id.navigation_calendar:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new CalendarFragment()).commit();
+                        break;
+                    case R.id.navigation_recommendation:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new RecommendationFragment()).commit();
+                        break;
+                    case R.id.navigation_setting:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new SettingFragment()).commit();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
